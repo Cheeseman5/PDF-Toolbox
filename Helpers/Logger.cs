28722c6ff32ca764488b0d45f1343884384bc6cf
David <dhuber04@gmail.com>
2022-04-04 22:20:29 -0500

    Broke out logging functionallity

---
+++ Helpers/Logger.cs
@@ -0,0 +1,85 @@
+﻿using PDFToolbox.Interfaces;
+
+namespace PDFToolbox.Helpers
+{
+    public class Logger : ILogger
+    {
+        public enum eDebuggerDetail
+        {
+            None = 0,
+            Assert,
+            Log,
+            Warning,
+            Error,
+            Fatal,
+            All
+        };
+
+        private eDebuggerDetail detail = eDebuggerDetail.All;
+
+        private const string ASSERT_PREFIX = "Assertion failed: ";
+        private const string LOG_PREFIX = "Log: ";
+        private const string WARN_PREFIX = "WARNING: ";
+        private const string ERR_PREFIX = "ERROR! ";
+
+        public Logger(eDebuggerDetail loggingDetail)
+        {
+            detail = loggingDetail;
+        }
+
+        private void Write(string prefix, object msg)
+        {
+            Console.WriteLine(prefix + msg as string);
+        }
+        private void Write(string prefix, object msg, params object[] args)
+        {
+            Write(prefix, string.Format(msg as string, args));
+        }
+
+        public void Assert(bool condition, object msg)
+        {
+            if ((int)detail >= (int)eDebuggerDetail.Assert)
+                if (!condition)
+                    Write(ASSERT_PREFIX, msg);
+        }
+        public void Assert(bool condition, object msg, params object[] args)
+        {
+            if ((int)detail >= (int)eDebuggerDetail.Assert)
+                if (!condition)
+                    Write(ASSERT_PREFIX, msg, args);
+        }
+
+        public void Log(object msg)
+        {
+            if ((int)detail >= (int)eDebuggerDetail.Log)
+                Write(LOG_PREFIX, msg);
+        }
+        public void Log(object msg, params object[] args)
+        {
+            if ((int)detail >= (int)eDebuggerDetail.Log)
+                Write(LOG_PREFIX, msg, args);
+        }
+
+        public void Warn(object msg)
+        {
+            if ((int)detail >= (int)eDebuggerDetail.Warning)
+                Write(WARN_PREFIX, msg);
+        }
+        public void Warn(object msg, params object[] args)
+        {
+            if ((int)detail >= (int)eDebuggerDetail.Warning)
+                Write(WARN_PREFIX, msg, args);
+        }
+
+        public void Error(object msg)
+        {
+            if ((int)detail >= (int)eDebuggerDetail.Error)
+                Write(ERR_PREFIX, msg);
+        }
+        public void Error(object msg, params object[] args)
+        {
+            if ((int)detail >= (int)eDebuggerDetail.Error)
+                Write(ERR_PREFIX, msg, args);
+        }
+    }
+}
---
+++ Interfaces/ILogger.cs
@@ -0,0 +1,14 @@
+﻿namespace PDFToolbox.Interfaces
+{
+    public interface ILogger
+    {
+        void Assert(bool condition, object msg);
+        void Assert(bool condition, object msg, params object[] args);
+        void Log(object msg);
+        void Log(object msg, params object[] args);
+        void Warn(object msg);
+        void Warn(object msg, params object[] args);
+        void Error(object msg);
+        void Error(object msg, params object[] args);
+    }
+}
