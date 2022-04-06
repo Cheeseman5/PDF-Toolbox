2007811bccaa609a911492e2896a5fa19eaa36e7
David <dhuber04@gmail.com>
2022-04-05 19:05:18 -0500

    Added Repositories proj

---
+++ Interfaces/ISystemFileRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.IO;
+
+namespace PDFToolbox.Interfaces
+{
+    public interface ISystemFileRepository
+    {
+        MemoryStream Load(string fPath);
+        //Models.Document LoadDocument(IO.FileIOInfo info);
+        //void SaveDocument(ViewModels.DocumentViewModel document);
+    }
+}
