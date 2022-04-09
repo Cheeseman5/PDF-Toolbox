8f791f6241524e8719ecc7aaeccf68fa63c8a254
David <dhuber04@gmail.com>
2022-04-09 14:14:04 -0500

    slimmed down DTOs

--- Models/Document.cs
+++ Models/Document.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using iTextSharp.text.pdf;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 using System.IO;
@@ -6,33 +7,23 @@ using System.Windows.Media.Imaging;
 
 namespace PDFToolbox.Models
 {
-    public class Document : Page
+    public class Document
     {
+        // image used to display in pageDict/document views
+        public BitmapImage image { get; set; }
+        public string fName { get; set; } = "";
+        public int originalPageNumber { get; set; }
+        public PdfNumber rotation { get; set; } = new PdfNumber(0f);
+        // Original rotation of pageDict (in degrees)
+        public PdfNumber originalRotation { get; set; } = new PdfNumber(0f);
+        // True = image is ignored on PDF creation; False = image is used
+        public bool isImagePreview { get; set; }
+        public Stream imageStream { get; set; }
+        public ObservableCollection<UIString> uiStrings { get; set; }
+        public int ID { get; set; }
+
         public FileStream document { get; set; }
-        //public List<PageViewModel> pages { get; set; }
-
-        public List<PageViewModel> pages { get; set; }
-
-        public Document()
-        {
-            pages = new List<PageViewModel>();
-            fName = "";
-        }
-
-        public void Rename(string newName, bool append=false)
-        {
-            try
-            {
-                newName = (append) ? Path.GetFileNameWithoutExtension(fName) + newName : newName;
-
-                fName = Path.GetDirectoryName(fName) + "\\" + newName + Path.GetExtension(fName);
-            }
-            catch
-            {
-                // may be a bad idea...
-                fName = newName;
-            }
-        }
+        public List<PageViewModel> pages { get; set; } = new List<PageViewModel>();
 
         public class PageViewModel
         {
--- Models/Page.cs
+++ Models/Page.cs
@@ -10,57 +10,18 @@ namespace PDFToolbox.Models
     {
         // image used to display in pageDict/document views
         public BitmapImage image { get; set; }
-        // Original document file name
         public string fName { get; set; }
-        // Original pageDict number
+        // Original page number
         public int number { get; set; }
-        // Rotation of pageDict (in degrees)
-        public PdfNumber rotation { get; set; }
+        public PdfNumber rotation { get; set; } = new PdfNumber(0f);
         // Original rotation of pageDict (in degrees)
-        public PdfNumber originalRotation { get; set; }
+        public PdfNumber originalRotation { get; set; } = new PdfNumber(0f);
         // True = image is ignored on PDF creation; False = image is used
         public bool isImagePreview { get; set; }
 
         public Stream imageStream { get; set; }
 
         public ObservableCollection<UIString> uiStrings { get; set; }
-
-        private static int nextID = 0;
-        public int id { get; private set; }
-
-        public Page()
-        {
-            id = nextID++;
-            rotation = new PdfNumber(0f);
-            originalRotation = new PdfNumber(0f);
-        }
-
-        public void Copy(Page page)
-        {
-            if (page == null) return;
-
-            this.image = page.image;
-            this.fName = page.fName;
-            this.number = page.number;
-            this.rotation = page.rotation;
-            this.originalRotation = page.originalRotation;
-            this.isImagePreview = page.isImagePreview;
-            this.imageStream = page.imageStream;
-            this.uiStrings = page.uiStrings;
-        }
-
-        public static Page MakeCopy(Page page)
-        {
-            Page p = new Page();
-
-            p.Copy(page);
-
-            return p;
-        }
-
-        public void SetRotation(float rotation)
-        {
-            this.rotation = new PdfNumber(rotation);
-        }
+        public int ID { get; set; }
     }
 }
