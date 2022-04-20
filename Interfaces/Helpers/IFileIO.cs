ee403961752ea4fe3f3ffcea0d16605cbb825516
David <dhuber04@gmail.com>
2022-04-19 20:31:29 -0500

    Added interface for FileIO

---
+++ Interfaces/Helpers/IFileIO.cs
@@ -0,0 +1,19 @@
+﻿using PDFToolbox.Models;
+//using System.Runtime.InteropServices.ComTypes;
+using System.Windows;
+
+namespace PDFToolbox.Interfaces.Helpers
+{
+    public interface IFileIO
+    {
+        //void RegisterStrategy(BaseIOStrategy strategy);
+        FileIOInfo[] ExtractFileInfo(System.Windows.IDataObject data);
+        Document[] ExtractDocument(FileIOInfo[] files);
+        Document[] ExtractDocument(IDataObject data);
+        Document LoadDocument(FileIOInfo info);
+        string ToTempFileName(string fPath);
+        string ToTempFileName(string fPath, int pageNumber);
+        bool IsFileValid(string fPath, bool checkFileExistance = false);
+        bool IsExtensionSupported(string file);
+    }
+}
