52e37efd7b8100c0a0dc859d5e54ba97d82f9627
David <dhuber04@gmail.com>
2022-04-23 14:01:42 -0500

    Applied interfaceto FileDrop Extractor

--- Helpers/FileDropExtractor.cs
+++ Helpers/FileDropExtractor.cs
@@ -1,16 +1,17 @@
 ﻿using System.Collections.Generic;
 using System.Windows;
 using System.IO;
+using PDFToolbox.Interfaces.Helpers;
 
 namespace PDFToolbox.Helpers
 {
-    public class FileDropExtractor : BaseFileIOExtractor
+    public class FileDropExtractor : IFileIOExtractor
     {
         public FileDropExtractor()
             : base()
         { }
 
-        public override FileIOInfo[] GetFileStreams(IDataObject data)
+        public Models.FileIOInfo[] GetFileStreams(IDataObject data)
         {
             if(data==null || !data.GetDataPresent(DataFormats.FileDrop)) return null;
 
