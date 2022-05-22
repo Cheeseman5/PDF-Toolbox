4a210eba63bb58afe74d6e5e619e95146932c387
Cheeseman5 <dhuber04@gmail.com>
2022-05-22 16:14:57 -0500

    Add config class

---
+++ Interfaces/IConfig.cs
@@ -0,0 +1,12 @@
+﻿namespace PDFToolbox.Interfaces
+{
+    public interface IConfig
+    {
+        string DefaultSaveDirectory { get; }
+        string DefaultTempSaveDirectory { get; }
+        string TempSavePrefix { get; }
+        string AppVersion { get; }
+        string AppName { get; }
+        bool SilenceMessagebox { get; }
+    }
+}
