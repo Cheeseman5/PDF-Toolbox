a32b682d849d0934f29e8c720ebd56e3debf3ea0
Cheeseman5 <dhuber04@gmail.com>
2022-06-13 16:25:06 -0500

    added interface for a document controller

---
+++ Interfaces/Controllers/IDocumentController.cs
@@ -0,0 +1,11 @@
+﻿using PDFToolbox.Models;
+
+namespace PDFToolbox.Interfaces.Controllers
+{
+    public interface IDocumentController
+    {
+        Document NewDocument();
+        Document CopyDocument(Document document);
+        Document AddPageToDocument(Document document, Page page);
+    }
+}
