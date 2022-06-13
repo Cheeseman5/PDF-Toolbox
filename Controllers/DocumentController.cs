00447609ae1f47dff80a387b0816ad76db0a30a5
Cheeseman5 <dhuber04@gmail.com>
2022-06-13 16:30:13 -0500

    added document controller

---
+++ Controllers/DocumentController.cs
@@ -0,0 +1,33 @@
+﻿using PDFToolbox.Interfaces.Controllers;
+using PDFToolbox.Interfaces.Factories;
+using PDFToolbox.Models;
+
+namespace PDFToolbox.Controllers
+{
+    public class DocumentController : IDocumentController
+    {
+        private IPageFactory _pageFactory;
+        public DocumentController(IPageFactory pageFactory)
+        {
+            _pageFactory = pageFactory;
+        }
+
+        public Document AddPageToDocument(Document document, Page page)
+        {
+            document.Pages.Add(page);
+            return document;
+        }
+
+        public Document CopyDocument(Document document)
+        {
+            Document doc = _pageFactory.CopyDocument(document);
+            return doc;
+        }
+
+        public Document NewDocument()
+        {
+            Document document = _pageFactory.CreateDocument();
+            return document;
+        }
+    }
+}
